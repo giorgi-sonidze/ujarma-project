73044818f31c685e4a112d39ff06185b5ce76e40
giorgi-sonidze <giorgisonidze8@gmail.com>
2025-02-02 18:12:54 +0400

    port updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import connection from './config/db.conf.js';
 import express from 'express'
 const app = express()
-const port = 3000
+const port = 3001
 // Middleware to parse JSON
 app.use(express.json());
 
