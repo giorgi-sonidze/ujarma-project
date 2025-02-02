e903f1212d129f326cb235bd44e98ef146ee503b
giorgi-sonidze <giorgisonidze8@gmail.com>
2025-02-02 18:52:51 +0400

    fixed database configuration

--- config/db.conf.js
+++ config/db.conf.js
@@ -1,10 +1,19 @@
 import mysql from 'mysql';
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'ujarma'
-})
 
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'ujarma'
+});
+// Test the connection
+(async () => {
+    try {
+        const result = await connection.query('SELECT 1'); // Simple test query
+        console.log('Database connected successfully!');
+    } catch (err) {
+        console.error('Database connection failed:', err.message);
+    }
+})();
 export default connection;
