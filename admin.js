c56bf8fe7e7588dd581cb7c2ad72b1c4f7eebd02
giorgi-sonidze <giorgisonidze8@gmail.com>
2025-02-03 21:35:52 +0400

    added post method in admin.js

--- admin.js
+++ admin.js
@@ -1,12 +1,12 @@
 import connection from './config/db.conf.js';
 import express from 'express'
 const app = express()
-const port = 3001
+const port = 3002
 // Middleware to parse JSON
 app.use(express.json());
 
 
-app.post('/news', (req, res) => {
+app.post('/addnews', (req, res) => {
     const { title, content, created_date} = req.body;
     const sql = 'INSERT INTO news (title, content, created_date) VALUES (?, ?, ?)';
     connection.query(sql, [title, content, created_date], (err, result) => {
@@ -14,5 +14,11 @@ app.post('/news', (req, res) => {
             return res.status(500).send('Error adding news');
         }
         res.status(201).send('news added successfully');
+        
     });
 });
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import connection from './config/db.conf.js';
 import express from 'express'
 const app = express()
-const port = 3001
+const port = 3002
 // Middleware to parse JSON
 app.use(express.json());
 
@@ -11,9 +11,25 @@ app.get('/', (req, res) => {
   res.send('Hello World!, hello georgia!')
 })
 
+
+app.get('/news', (req, res) => {
+  connection.query('SELECT * FROM news', (err, results) => {
+    if (err) {
+      console.error("Error fetching news:", err);
+      return res.status(500).json({ error: "Database query failed" });
+    }
+    res.json(results); // Send the fetched news as JSON
+  });
+});
+
+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
 
 
 
