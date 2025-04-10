c396a62682239788381e4fdbce4a33e9c26e7305
giorgi-sonidze <giorgisonidze8@gmail.com>
2025-04-10 04:36:58 +0400

    content type added

--- clientinfo.js
+++ clientinfo.js
@@ -1,22 +1,26 @@
 export default {
-    logger: (app, connection) => (req, res, next) => {
-      const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-      const userAgent = req.headers['user-agent'];
-      const contentType = req.headers['content-type'];
-     
-  
-      const sql = "INSERT INTO clientinfo (client_ip, user_agent, content_type, connection) VALUES (?, ?, ?)";
-      const values = [clientIp, userAgent, contentType];
-  
-      connection.query(sql, values, (err, result) => {
-        if (err) {
-          console.error("Error inserting client info:", err);
-          // Optionally handle DB errors (don't stop request flow unless critical)
-        } else {
-          console.debug("Client info saved:", result.insertId);
-        }        
-      });
-      next();
-    }
+  logger: (app, connection) => (req, res, next) => {
+    const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const userAgent = req.headers['user-agent'];
+    const contentType = req.headers['content-type'];
+
+
+    const sql = "INSERT INTO clientinfo (client_ip, user_agent, content_type, connection) VALUES (?, ?, ?)";
+    const values = [clientIp, userAgent, contentType];
+
+    connection.query(sql, values, (err, result) => {
+      if (err) {
+        console.error("Error inserting client info:", err);
+        // Optionally handle DB errors (don't stop request flow unless critical)
+      } else {
+        console.debug("Client info saved:", result.insertId);
+      }
+    });
+
+    app.get('/manual-json', (req, res) => {
+      res.set('Content-Type', 'application/json');
+      res.send(JSON.stringify({ message: 'Hello from JSON!' }));
+    });
+    next();
   }
-  
+}
